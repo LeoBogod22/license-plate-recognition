7f9dd4b2401f915ab919a79dec9b94883732bae2
Leobogod22 <combatstriker111@gmail.com>
2020-09-01 20:12:28 -0400

    year make and brand detected

--- detect_face_video.py
+++ detect_face_video.py
@@ -2,7 +2,9 @@ import matplotlib.pyplot as plt
 import cv2
 
 #import imutils
-
+import requests
+import base64
+import json
 import numpy as np
 from PIL import Image
 from PIL import ImageEnhance
@@ -11,77 +13,87 @@ from scipy.signal import convolve2d
 import pytesseract
 import PIL.ImageOps
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
-img = cv2.imread('f.jpg',cv2.IMREAD_COLOR)
-img = cv2.resize(img, (600,400) )
-threshold = 180 # to be determined
-_, img_binarized = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
-pil_img = Image.fromarray(img_binarized)
 
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-gray = cv2.bilateralFilter(gray, 13, 15, 15)
-
-edged = cv2.Canny(gray, 30, 200)
-thresh = cv2.adaptiveThreshold(gray, 255, 1, 1, 11, 2)
-#contours = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-#contours = imutils.grab_contours(contours)
-contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
-screenCnt = None
-
-gaussian_blur_license_plate = cv2.GaussianBlur(
-    img, (5, 5), 0)
-
-for c in contours:
-
-    peri = cv2.arcLength(c, True)
-    approx =    rect = cv2.minAreaRect(c)
-
-    if len(approx) == 4:
-        screenCnt = approx
-        break
-
-if screenCnt is None:
-    detected = 0
-    print ("No contour detected")
-else:
-     detected = 1
-
-if detected == 1:
-    cv2.drawContours(img, [screenCnt], -1, (0, 0, 255), 3)
-
-mask = np.zeros(gray.shape,np.uint8)
-new_image = cv2.drawContours(mask,[screenCnt],0,255,-1,)
-new_image = cv2.bitwise_and(img,img,mask=mask)
-
-(x, y) = np.where(mask == 255)
-(topx, topy) = (np.min(x), np.min(y))
-(bottomx, bottomy) = (np.max(x), np.max(y))
-Cropped = gray[topx:bottomx+1, topy:bottomy+1]
-
-text = pytesseract.image_to_string(Cropped, config='--psm 11')
-print("programming_fever's License Plate Recognition\n")
-print("Detected license plate Number is:",text)
-img = cv2.resize(img,(500,300))
-
-Cropped = cv2.resize(Cropped,(400,200))
+plate=None
+def main (img):
 
 
-im = Image.fromarray(Cropped)
-im.save('test.png')
+
+    img = cv2.imread(img,cv2.IMREAD_COLOR)
+
+    img = cv2.resize(img, (600,400) )
+    img = cv2.resize(img, (600,400) )
+    threshold = 180 # to be determined
+    _, img_binarized = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
+    pil_img = Image.fromarray(img_binarized)
 
 
-image = Image.open('test.png')
-enh_bri = ImageEnhance.Brightness(image )
-brightness = 1.0
-image_brightened = enh_bri.enhance(brightness)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.bilateralFilter(gray, 13, 15, 15)
+
+    edged = cv2.Canny(gray, 30, 200)
+    thresh = cv2.adaptiveThreshold(gray, 255, 1, 1, 11, 2)
+    #contours = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    #contours = imutils.grab_contours(contours)
+    contours = sorted(contours, key = cv2.contourArea, reverse = True)[:30]
+    screenCnt = None
+
+    gaussian_blur_license_plate = cv2.GaussianBlur(
+        img, (5, 5), 0)
+
+    for c in contours:
+
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+        if len(approx) == 4:
+            screenCnt = approx
+
+            break
+
+    if screenCnt is None:
+        detected = 0
+        print ("No contour detected")
+    else:
+         detected = 1
+
+    if detected == 1:
+        cv2.drawContours(img, [screenCnt], -1, (0, 0, 255), 3)
+
+    mask = np.zeros(gray.shape,np.uint8)
+    new_image = cv2.drawContours(mask,[screenCnt],0,255,-1,)
+    new_image = cv2.bitwise_and(img,img,mask=mask)
+
+    (x, y) = np.where(mask == 255)
+    (topx, topy) = (np.min(x), np.min(y))
+    (bottomx, bottomy) = (np.max(x), np.max(y))
+    Cropped = gray[topx:bottomx+1, topy:bottomy+1]
+
+    text = pytesseract.image_to_string(Cropped, lang='eng')
+    print("programming_fever's License Plate Recognition\n")
+    print("Detected license plate Number is:",text)
+    img = cv2.resize(img,(500,300))
+
+    Cropped = cv2.resize(Cropped,(400,200))
 
 
-imwhole = np.array(image_brightened)
+    im = Image.fromarray(Cropped)
+    im.save('test.png')
 
 
-cv2.imshow('car',img)
-cv2.imshow('Cropped',imwhole)
+    image = Image.open('test.png')
+    enh_bri = ImageEnhance.Brightness(image )
+    brightness = 1.0
+    image_brightened = enh_bri.enhance(brightness)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+    imwhole = np.array(image_brightened)
+
+
+    cv2.imshow('car',img)
+    cv2.imshow('Cropped',imwhole)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
--- f.py
+++ f.py
@@ -1,30 +1,143 @@
-import numpy as np
-import cv2
-import pytesseract
-import matplotlib.pyplot as plt
+import PySimpleGUI as sg
+# import PySimpleGUIQt as sg
+import os.path
+import PIL.Image
+import io
+#import imutils
+import requests
+import base64
+import json
+import base64
+from detect_face_video  import main
+"""
 
-img = cv2.imread('at.jpg')
-#convert my image to grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#perform adaptive threshold so that I can extract proper contours from the image
-#need this to extract the name plate from the image.
-thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
-contours,h = cv2.findContours(thresh,1,2)
+"""
 
-#once I have the contours list, i need to find the contours which form rectangles.
-#the contours can be approximated to minimum polygons, polygons of size 4 are probably rectangles
-largest_rectangle = [0,0]
-for cnt in contours:
-    approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
-    if len(approx)==4: #polygons with 4 points is what I need.
-        area = cv2.contourArea(cnt)
-        if area > largest_rectangle[0]:
-            #find the polygon which has the largest size.
-            largest_rectangle = [cv2.contourArea(cnt), cnt, approx]
+plate='f'
+Brand='l'
+year='0'
+Color='b'
+def convert_to_bytes(file_or_bytes, resize=None):
+    '''
+    Will convert into bytes and optionally resize an image that is a file or a base64 bytes object.
+    Turns into  PNG format in the process so that can be displayed by tkinter
+    :param file_or_bytes: either a string filename or a bytes base64 image object
+    :type file_or_bytes:  (Union[str, bytes])
+    :param resize:  optional new size
+    :type resize: (Tuple[int, int] or None)
+    :return: (bytes) a byte-string object
+    :rtype: (bytes)
+    '''
+    if isinstance(file_or_bytes, str):
+        img = PIL.Image.open(file_or_bytes)
+    else:
+        try:
+            img = PIL.Image.open(io.BytesIO(base64.b64decode(file_or_bytes)))
+        except Exception as e:
+            dataBytesIO = io.BytesIO(file_or_bytes)
+            img = PIL.Image.open(dataBytesIO)
 
-x,y,w,h = cv2.boundingRect(largest_rectangle[1])
-#crop the rectangle to get the number plate.
-roi=img[y:y+h,x:x+w]
-#cv2.drawContours(img,[largest_rectangle[1]],0,(0,0,255),-1)
-plt.imshow(roi, cmap = 'gray')
-plt.show()
+    cur_width, cur_height = img.size
+    if resize:
+        new_width, new_height = resize
+        scale = min(new_height/cur_height, new_width/cur_width)
+        img = img.resize((int(cur_width*scale), int(cur_height*scale)), PIL.Image.ANTIALIAS)
+    with io.BytesIO() as bio:
+        img.save(bio, format="PNG")
+        del img
+        return bio.getvalue()
+
+def update_plate(filename):
+
+        SECRET_KEY = 'sk_ed3d203bf4c9a7c2910ec0c0'
+
+        with open(filename, 'rb') as image_file:
+            img_base64 = base64.b64encode(image_file.read())
+
+        url = 'https://api.openalpr.com/v3/recognize_bytes?recognize_vehicle=1&country=us&secret_key=%s' % (SECRET_KEY)
+        r = requests.post(url, data = img_base64)
+
+
+
+        try:
+             global plate
+             plate=r.json()['results'][0]['plate'],
+             global Brand
+             Brand= r.json()['results'][0]['vehicle']['make_model'][0]['name'],
+             global Color
+             Color=  r.json()['results'][0]['vehicle']['color'][0]['name'],
+             global year
+             year =  r.json()['results'][0]['vehicle']['year'][0]['name'],
+             print(plate)
+
+        except:
+              print ('error')
+
+# --------------------------------- Define Layout ---------------------------------
+
+# First the window layout...2 columns
+
+sg.theme('Dark Blue 3')
+left_col = [[sg.Text('Folder'), sg.In(size=(25,1), enable_events=True ,key='-FOLDER-'), sg.FolderBrowse()],
+            [sg.Listbox(values=[], enable_events=True, size=(40,20),key='-FILE LIST-')],
+            [sg.Text('Resize to'), sg.In(key='-W-', size=(5,1)), sg.In(key='-H-', size=(5,1))],
+            [sg.Button("Resize", button_color=("white", "blue"), size=(6, 1))]]
+
+# For now will only show the name of the file that was chosen
+images_col = [[sg.Text('You choose from the list:')],
+              [sg.Text(size=(40,1), key='-TOUT-')],
+              [sg.Image(key='-IMAGE-')]]
+
+# ----- Full layout -----
+layout = [[sg.Column(left_col, element_justification='c'),  sg.VSeperator(),sg.Column(images_col, element_justification='c')]]
+
+
+# --------------------------------- Create Window ---------------------------------
+window = sg.Window('Multiple Format Image Viewer', layout,resizable=True)
+
+# ----- Run the Event Loop -----
+# --------------------------------- Event Loop ---------------------------------
+while True:
+    event, values = window.read()
+
+
+    if event in (sg.WIN_CLOSED, 'Exit'):
+        break
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+    if event == '-FOLDER-':                         # Folder name was filled in, make a list of files in the folder
+        folder = values['-FOLDER-']
+        try:
+            file_list = os.listdir(folder)         # get list of files in folder
+        except:
+            file_list = []
+        fnames = [f for f in file_list if os.path.isfile(
+            os.path.join(folder, f)) and f.lower().endswith((".png", ".jpg", "jpeg", ".tiff", ".bmp"))]
+        window['-FILE LIST-'].update(fnames)
+    if event == 'Resize':
+        window['-IMAGE-'].update(data=convert_to_bytes(filename, resize=new_size))
+    elif event == '-FILE LIST-':    # A file was chosen from the listbox
+        try:
+            filename = os.path.join(values['-FOLDER-'], values['-FILE LIST-'][0])
+
+            window['-TOUT-'].update(filename)
+
+
+            if values['-W-'] and values['-H-']:
+                new_size = int(values['-W-']), int(values['-H-'])
+
+            else:
+                new_size = None
+
+            window['-IMAGE-'].update(data=convert_to_bytes(filename, resize=new_size))
+
+            main(filename)
+            update_plate(filename)
+            sg.Popup("license plate" , plate , "color " , Color , "year ",  year ,"brand " , Brand , keep_on_top=True)
+
+            print(plate)
+        except Exception as E:
+            print(f'** Error {E} **')
+            pass        # something weird happened making the full filename
+# --------------------------------- Close & Exit ---------------------------------
+window.close()
